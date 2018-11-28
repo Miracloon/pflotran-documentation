53984577be52f753becea846da107932fa4622ce
Glenn Hammond <gehammo@sandia.gov>
2018-11-27 16:59:48 -0800

    Fixed exit status; added verbosity

--- tools/build.sh
+++ tools/build.sh
@@ -15,6 +15,7 @@ make html 2>&1 | tee $MAKE_LOG
 NUM_ERROR=$(grep -c "ERROR:" "$MAKE_LOG")
 NUM_WARNING=$(grep -c "WARNING:" "$MAKE_LOG")
 NUM_TOTAL=`expr $NUM_ERROR + $NUM_WARNING`
+exit_status = 0
 if [ $NUM_TOTAL -eq 0 ]; then
   echo 'build succeeded'
   echo 'building tarball'
@@ -28,17 +29,21 @@ if [ $NUM_TOTAL -eq 0 ]; then
   fi
 else
   echo
-  echo 'build failed due to the following warnings or errors:'
+  echo 'build failed due to the following warnings or errors'
   if [ $NUM_ERROR -gt 0 ]; then
     echo
     echo 'Errors:'
+    echo
     grep 'ERROR:' $MAKE_LOG
   fi
   if [ $NUM_WARNING -gt 0 ]; then
     echo
     echo 'Warnings:'
+    echo
     grep 'WARNING:' $MAKE_LOG
   fi
+  echo
+  echo 'build failed due to the preceding warnings or errors'
   echo
   exit_status = 1
 fi
