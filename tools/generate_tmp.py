33a29774819ee19d6b5c1f7e9f6a7d0621569de3
Glenn Hammond <gehammo@sandia.gov>
2020-04-20 11:57:45 -0700

    Added EXPERT option

--- tools/generate_tmp.py
+++ tools/generate_tmp.py
@@ -19,53 +19,78 @@ filenames.pop(0) # remove outfilename
 print('Generating {}'.format(outfilename.split('/')[-1]))
      
 dictionary = {}
+expert_dictionary = {}
 for filename in filenames:
     f = open(filename,'r')
     list_ = []
     strings = []
+    skip = False
+    expert = False
     for line in f:
-#        print(line)
         if line.startswith('#'):
             continue
         if len(line.rstrip()) > 0 and not line.startswith(' '):
             if list_ and not skip:
+                keyword = list_[0]
                 list_.append(strings)
-#                print(list_)
-                if list_[0] in dictionary:
+                if keyword in dictionary or keyword in expert_dictionary:
                     sys.exit('Key {} already exists in Python dictionary. '.
-                             format(list_[0])+
+                             format(keyword)+
                              'Currently constructing {}.'.format(outfilename))
-                dictionary[list_[0]] = list_
+                if expert:
+                    exert_dictionary[keyword] = list_
+                else:
+                    dictionary[keyword] = list_
             list_ = []
             strings = []
             skip = False
+            expert = False
         if not list_ and len(line.strip()) > 0:
             w = line.split()
             list_.append(w[0].upper())
             list_.append(line.strip())
         elif line.strip().startswith('@'):
             skip = True
-            applicable_modes = line.strip().strip('@').split()
-            for local_mode in applicable_modes:
-                if not local_mode in supported_modes:
-                    sys.exit('Local mode {} is not a '.format(local_mode)+
-                             'supportd mode in generate_tmp.py.')
-                if local_mode == mode:
-                    skip = False
-                    break
+            w = line.strip().strip('@').split()
+            keyword = w[0].strip()
+            w = w[1:]
+            if keyword.strip().startswith('MODES'):
+                applicable_modes = w
+                for local_mode in applicable_modes:
+                    if not local_mode in supported_modes:
+                        sys.exit('Local mode {} is '.format(local_mode)+
+                                 'not a supportd mode in generate_tmp.py.')
+                    if local_mode == mode:
+                        skip = False
+                        break
+            elif keyword.startswith('EXPERT'):
+                expert = True
         else:
             strings.append(line.strip())
     if len(strings) > 0:
         if list_ and not skip:
             list_.append(strings)
-            dictionary[list_[0]] = list_
+            keyword = list_[0]
+            if keyword in dictionary or keyword in expert_dictionary:
+                sys.exit('Key {} already exists in Python dictionary. '.
+                         format(keyword)+
+                         'Currently constructing {}.'.format(outfilename))
+            if expert:
+                exert_dictionary[keyword] = list_
+            else:
+                dictionary[keyword] = list_
     f.close()
     
 f = open(outfilename,'w')
 for entry in sorted(dictionary):
-#    print(entry)
     f.write('{}\n'.format(dictionary[entry][1].strip()))
     strings = dictionary[entry][2]
+    for string in strings:
+      f.write(' {}\n'.format(string.strip()))
+f.write('\n**Expert Settings**\n'.format(dictionary[entry][1].strip()))
+for entry in sorted(expert_dictionary):
+    f.write('{}\n'.format(expert_dictionary[entry][1].strip()))
+    strings = expert_dictionary[entry][2]
     for string in strings:
       f.write(' {}\n'.format(string.strip()))
 f.close()
