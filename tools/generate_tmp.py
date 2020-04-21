1c63256cd523af50eabd6fdcf4063946f50213da
Glenn Hammond <gehammo@sandia.gov>
2020-04-20 17:04:42 -0700

    try 4

--- tools/generate_tmp.py
+++ tools/generate_tmp.py
@@ -22,7 +22,7 @@ basic_dictionary = {}
 expert_dictionary = {}
 for filename in filenames:
     try: 
-        f = open(filename,'rt')
+        f = open(filename,encoding='utf-8',mode='r')
     except IOError:
         sys.exit('Unable to open file {}.'.format(filename))
     list_ = []
